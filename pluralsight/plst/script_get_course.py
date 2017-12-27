1532bb260098b1915c801bc4f1db3cf616423d14
Zhuoyi Huang <joeyfuerimmer@gmail.com>
2017-12-27 01:45:21 -0600

    script_get_course.py use finally to ensure the browser close at failure

--- pluralsight/plst/script_get_course.py
+++ pluralsight/plst/script_get_course.py
@@ -15,32 +15,33 @@ def get_course(ncourse_max):
     session.login(driver, home_dir + '/plst.credential.json')
     utils.wait(3)
 
-    ncourse = 0
-    for line in open('courses.list'):
-        if line.startswith('#'):
-            continue
-        course_id = line.rstrip()
-        out_dir = 'courses/' + course_id
-        fname_json = '{}/{}.json'.format(out_dir, course_id)
-        if os.path.isfile(fname_json):
-            continue
-        utils.print_message('get course "{}" ({}/{})'.format(course_id, ncourse+1, ncourse_max))
-        c = course.Course(driver, cache_dir=cache_dir, course_id=course_id)
-        c.get_description()
-        c.get_exercise_files(out_dir)
-        c.get_table_of_content()
-        c.get_transcript()
-        c.format_json(fname_json)
-        ncourse += 1
-        utils.print_message('----------------------------------------------')
+    try:
+        ncourse = 0
+        for line in open('courses.list'):
+            if line.startswith('#'):
+                continue
+            course_id = line.rstrip()
+            out_dir = 'courses/' + course_id
+            fname_json = '{}/{}.json'.format(out_dir, course_id)
+            if os.path.isfile(fname_json):
+                continue
+            utils.print_message('get course "{}" ({}/{})'.format(course_id, ncourse+1, ncourse_max))
+            c = course.Course(driver, cache_dir=cache_dir, course_id=course_id)
+            c.get_description()
+            c.get_exercise_files(out_dir)
+            c.get_table_of_content()
+            c.get_transcript()
+            c.format_json(fname_json)
+            ncourse += 1
+            utils.print_message('----------------------------------------------')
+            utils.wait(3)
+
+            if ncourse == ncourse_max:
+                break
+    finally:
+        session.logout(driver)
         utils.wait(3)
-
-        if ncourse == ncourse_max:
-            break
-
-    session.logout(driver)
-    utils.wait(3)
-    utils.close_driver(driver)
+        utils.close_driver(driver)
 
 
 ncourse_max = int(sys.argv[1])
