f9405b561928b80eb36ef1af167d129203d59e8f
Zhuoyi Huang <joeyfuerimmer@gmail.com>
2017-12-30 04:49:41 -0600

    add script to get url of all filter options

---
+++ pluralsight/pluralsight/script_get_search_options.py
@@ -0,0 +1,42 @@
+import utils
+import session
+import search
+import sys
+import os
+import pathlib
+
+
+def get_search_options():
+
+    driver = utils.start_driver('chrome')
+    home_dir = str(pathlib.Path.home())
+    session.login(driver, home_dir + '/plst.credential.json')
+    utils.wait(3)
+
+    filt_name_dict = {
+        'level': 'SKILL LEVELS',
+        'role': 'ROLES',
+        'subject': 'SUBJECTS TO LEARN',
+        'tool': 'TOOLS',
+        'cert': 'CERTIFICATIONS',
+        'author': 'AUTHORS',
+    }
+
+    try:
+
+        for filt, filt_name in sorted(filt_name_dict.items()):
+            opt_url_dict = search.get_filter_options_url(driver, filt_name)
+            utils.save_json(opt_url_dict, 'search/filt_{}.json'.format(filt))
+            utils.wait(10)
+
+    finally:
+        session.logout(driver)
+        utils.wait(3)
+        utils.close_driver(driver)
+
+
+get_search_options()
+
+
+
+# load_course_video_list(sys.argv[1])
--- pluralsight/pluralsight/search.py
+++ pluralsight/pluralsight/search.py
@@ -1,102 +1,84 @@
 import utils
-import session
-import sys
-import imp
-import time
+
+search_url = 'https://app.pluralsight.com/library/search'
+
+def get_filter_options_url(driver, filt_name):
+    # filt_name = 'SKILL LEVELS', 'ROLES', 'SUBJECTS TO LEARN'
+    # 'TOOLS', 'CERTIFICATIONS', 'AUTHORS'
+    # reload the search url
+    utils.print_message('get options of filter {} ...'.format(filt_name))
+    utils.open_url(driver, search_url, reopen=True)
+
+    opt_url_dict = dict()
+    for filt in driver.find_elements_by_xpath(
+            '//li[starts-with(@class, "facet__section ")]'):
+        if filt_name != filt.find_element_by_xpath('./div/h3').text:
+            continue
+
+        # expand option list
+        if filt.get_attribute('class') == 'facet__section l-search__facets-list--item':
+            filt.find_element_by_xpath('./div/div').click()
+
+        # get all options
+        for opt in filt.find_elements_by_xpath('.//a[@role="checkbox"]'):
+            opt_name= opt.get_attribute('aria-label')
+            opt_url = opt.get_attribute('href')
+            opt_url_dict[opt_name] = opt_url
+
+    utils.print_message('found urls of {} options'.format(len(opt_url_dict)))
+
+    return opt_url_dict
 
 
-host = 'https://app.pluralsight.com'
+def get_all_courses_per_option(driver, opt_url):
+
+    utils.open_url(driver, opt_url, reopen=True)
+    switch_to_courses(driver, 'Courses')
+    ncourse = find_number_courses(driver)
+    load_all_courses(driver)
+    course_id_list = get_course_ids(driver)
+    if ncourse != len(course_id_list):
+        utils.print_message('*ERROR*: number of courses mismatch, expected {}, loaded {}'.format(ncourse, len(course_id_list)))
+        raise
+    return course_id_list
 
 
-def load_all_courses(driver, html_name, num_load=None):
 
-    utils.open_url(driver, host + '/library/search')
-
-    # switch to Course tab
-    for elem in driver.find_elements_by_xpath('//li[@class="tab-list__item"]'):
-        if elem.text == 'Courses':
-            elem.click()
-
-    # define target scraping section
-    course_section = driver.find_element_by_xpath(
-        '//div[@aria-selected="true"]')
-
-    # expected number
-    ncourse_expect = int(course_section.find_element_by_xpath(
-        './/*[@class="l-search__results-page-info"]').text.split()[1])
-
-    nload = 0
-    if num_load:
-        nload_max = num_load
-    else:
-        nload_max = (ncourse_expect // 25) + 3
-
-    while nload < nload_max:
-        courses = course_section.find_elements_by_xpath(
-            './/li[@class="courses-list__item"]')
-        ncourses = len(courses)
-        utils.print_message('#load={}, ncourses={}'.format(nload, ncourses))
-
-        nload += 1
-        buttons = course_section.find_elements_by_xpath(
-            './/a[@class="button button--outlined"]')
-        if len(buttons) == 0:
+def switch_to_courses(driver, tab_name):
+    # tab_name = 'All', 'Courses', 'Paths'
+    for tab in driver.find_elements_by_xpath('//li[starts-with(@class, "tab-list__item")]'):
+        if tab.text == tab_name:
+            tab.click()
             break
 
-        buttons[0].click()
-        utils.wait(3)
 
-    # save html
-    utils.save_html(driver, html_name)
-
-    course_list = course_section.find_elements_by_xpath(
-        './/li[@class="courses-list__item"]')
-    utils.print_message('expect {} courses, loaded {}.'.format(
-        ncourse_expect, len(course_list)))
-
-
-def get_all_courses(driver, json_name):
-
-    course_section = driver.find_element_by_xpath(
-        '//div[@aria-selected="true"]')
-    course_list = course_section.find_elements_by_xpath(
-        './/li[@class="courses-list__item"]')
-
-    out_list = []
-    ncourse = 0
-    for course in course_list:
-        out = {}
-        course_header = course.find_element_by_xpath(
-            './/*[@class="courses-list__item-headers"]/a')
-        out['title'] = course_header.get_attribute('title')
-        out['url'] = course_header.get_attribute('href')
-        course_meta = course.find_element_by_xpath(
-            './/*[@class="courses-list__item-meta"]')
-        out['author'] = course_meta.find_element_by_xpath(
-            './p[@class="courses-list__item-authors"]/span/a').text
-        out['level'] = course_meta.find_element_by_xpath(
-            './p[@class="courses-list__item-level"]').text
-        out['date'] = course_meta.find_element_by_xpath(
-            './time[@class="courses-list__item-date"]').text
-        out['level'] = course_meta.find_element_by_xpath(
-            './time[@class="courses-list__item-duration"]').text
-        ncourse += 1
-        out_list.append(out)
-
-    utils.save_json(out_list, json_name)
-
-
-if __name__ == '__main__':
-
-    import session
-
-    driver = utils.start_driver('chrome')
-    out_json = sys.argv[1]
-    out_html = out_json.rsplit('.',1)[0] + '.html'
+def find_number_courses(driver):
+    header = driver.find_element_by_xpath('//span[starts-with(@class,"searchHeader---")]')
     try:
-        session.login(driver, 'input/credential.json')
-        load_all_courses(driver, out_html, num_load=None)
-        get_all_courses(driver, out_json)
+        ncourse = int(header.text.split()[1])
+    except:
+        ncourse = 0
+    return ncourse
 
-    finally:
-        utils.close_driver(driver)
+
+def load_all_courses(driver):
+    switch_to_courses(driver, 'Courses')
+    while True:
+        try:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.find_element_by_xpath('//a[@class="button button--outlined"]').click()
+            utils.wait(3)
+        except:
+            break
+    driver.execute_script("window.scrollTo(0, 0);")
+
+
+def get_course_ids(driver):
+
+    course_id_list = []
+    for course in driver.find_elements_by_xpath('//div[@class="l-search__results__primary"]/ol/div'):
+        course_url = course.find_element_by_xpath('.//a[starts-with(@href, "/library/courses/")]')
+        course_id = course_url.get_attribute('href').split('/')[-1]
+        course_id_list.append(course_id)
+
+    return course_id_list
