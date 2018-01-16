bfc58d45f1682702c14ef6e7121cd3795416fcf2
joeyfuerimmer@gmail.com <joeyfuerimmer@gmail.com>
2018-01-15 23:11:25 -0600

    add pyvirtualdisplay and progress prints, still use chrome, tested with crontab

--- classicalarchives/classicalarchives/script_get_midi.py
+++ classicalarchives/classicalarchives/script_get_midi.py
@@ -5,12 +5,19 @@ import session
 import sys
 import download
 import pathlib
+import time
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(1024,768))
+display.start()
 
+utils.print_message('---------- Start Time: {} ----------'.format(time.ctime()))
 
 driver = utils.start_driver('chrome')
+# driver = utils.start_driver('phantomjs')
 home_dir = str(pathlib.Path.home())
 session.login(driver, home_dir + '/cma.credential.json')
 utils.wait(3)
+utils.print_message(' ')
 
 ntrack = 0
 ntrack_max = 103
@@ -57,3 +64,6 @@ utils.print_message(' ')
 session.logout(driver)
 utils.wait(3)
 utils.close_driver(driver)
+
+utils.print_message('---------- Finish Time: {} ----------'.format(time.ctime()))
+display.stop()
