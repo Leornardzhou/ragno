d7d9d5785662d3e279a6b2c26930142825540ef4
joeyfuerimmer@gmail.com <joeyfuerimmer@gmail.com>
2018-03-18 03:53:33 -0500

    black_list.py: filter proxies not chaning IP

---
+++ proxy/script/black_list.py
@@ -0,0 +1,53 @@
+'''This program create a list of proxies which are not effectively changing ip.
+'''
+
+import os
+import json
+import gzip
+import sys
+import requests
+
+
+def black_list(json_name, out_bl_name):
+
+
+    r = requests.get('http://icanhazip.com')
+    here = r.text.rstrip().rsplit('.',2)[0] + '.'
+
+    print('reading '+json_name)
+    data = json.loads(gzip.open(json_name).read().decode('utf-8'))
+
+    blist = set()
+    ndata = len(data)
+    for i,x in enumerate(sorted(data)):
+        print('testing proxy ({}/{}) {} ... '
+              .format(i+1, ndata, x), end='', flush=True)
+        try:
+            r = requests.get('http://icanhazip.com', proxies={'http':x},
+                             timeout=(0.1, 0.1))
+            if r.text.startswith(here):
+                print('black!', end='', flush=True)
+                blist.add(x)
+        except:
+            pass
+        print(flush=True)
+
+    print('writing ' + out_bl_name)
+    fout = open(out_bl_name, 'w')
+    for x in sorted(blist):
+        print(x, file=fout)
+    fout.close()
+
+
+if __name__ == '__main__':
+
+    for fin in sorted(os.listdir('../data')):
+        if fin.endswith('.proxies.json.gz') and fin.startswith('proxydocker'):
+            timestamp = fin.split('.')[1]
+            out_bl_name = ('../data/proxydocker.{}.proxies.blacklist'
+                           .format(timestamp))
+            fin = '../data/' + fin
+            if os.path.isfile(out_bl_name):
+                continue
+            black_list(fin, out_bl_name)
+            print()
