c097437869620bd8800a3a907a60b754e3fe6a86
joeyfuerimmer@gmail.com <joeyfuerimmer@gmail.com>
2018-04-14 20:30:08 -0500

    scrape.py: update to crawl all URLs of hidden journals

--- bioxio/script/scrape.py
+++ bioxio/script/scrape.py
@@ -14,13 +14,11 @@ host = 'www.bioxbio.com'
 def get_journal_data(in_url, out_json, tsleep):
     '''Get journal data (name, issn, impact factors) from input URL
     http://www.bioxbio.com/if/html/{journal}.html
+    and get URL of next/previous journals
     '''
 
-    data = {}
+    data = {'done': [], 'todo': []}
     error = None
-
-    if os.path.isfile(out_json):
-        return data, error
 
     try:
         utils.qprint('get ' + in_url)
@@ -28,41 +26,53 @@ def get_journal_data(in_url, out_json, tsleep):
         if not r.ok:
             raise
         time.sleep(tsleep)
-
         root = etree.HTML(r.text)
 
-        data['url'] = in_url
-        data['title'] = root.xpath('//div/h1')[0].text
-        x = re.findall(r'<p>Journal Abbreviation: (.*?)<br>', r.text)
-        data['title_abbrev'] = x[0] if x else x
-        x = re.findall(r'Journal ISSN: (.*?)</p>', r.text)
-        data['issn'] = x[0] if x else x
+        if not os.path.isfile(out_json):
 
-        data['impact'] = {}
-        for tr in root.xpath('//table/tr'):
-            td_list = tr.xpath('./td')
-            if len(td_list) != 3:
-                continue
-            year, ifact, npub = [x.text for x in td_list]
-            if year == 'Year':
-                continue
-            try:
-                ifact = float(ifact)
-            except:
-                ifact = -1
-            try:
-                npub = int(npub)
-            except:
-                npub = -1
-            data['impact'][year] = {
-                'ifact': ifact, 'npub': npub,
-            }
+            jdata = {}
+            jdata['url'] = in_url
+            jdata['title'] = root.xpath('//div/h1')[0].text
+            x = re.findall(r'<p>Journal Abbreviation: (.*?)<br>', r.text)
+            jdata['title_abbrev'] = x[0] if x else x
+            x = re.findall(r'Journal ISSN: (.*?)</p>', r.text)
+            jdata['issn'] = x[0] if x else x
 
-        utils.write_json(data, out_json)
+            jdata['impact'] = {}
+            for tr in root.xpath('//table/tr'):
+                td_list = tr.xpath('./td')
+                if len(td_list) != 3:
+                    continue
+                year, ifact, npub = [x.text for x in td_list]
+                if year == 'Year':
+                    continue
+                try:
+                    ifact = float(ifact)
+                except:
+                    ifact = -1
+                try:
+                    npub = int(npub)
+                except:
+                    npub = -1
+                jdata['impact'][year] = {
+                    'ifact': ifact, 'npub': npub,
+                }
+
+            utils.write_json(jdata, out_json)
+
+        data['done'].append(in_url)
+
+        # get prev and next url
+        a = root.xpath('//div[@class="col-md-6 col-sm-12 text-left"]/a')
+        data['todo'].append(
+            'http://www.bioxbio.com/if/html/' + a[0].get('href'))
+        a = root.xpath('//div[@class="col-md-6 col-sm-12 text-right"]/a')
+        data['todo'].append(
+            'http://www.bioxbio.com/if/html/' + a[0].get('href'))
 
     except Exception as e:
         error = '*ERROR* ' + str(e)
-        data = {}
+        data = []
 
     return data, error
 
@@ -160,20 +170,116 @@ def get_all_journal_data(all_journal_dict, out_dir,
     args_list = []
     for subject in sorted(all_journal_dict):
         for url in sorted(all_journal_dict[subject]):
-            journal_abbrev = re.findall(r'/if/html/(.*?).html', url)[0].lower()
+            journal_abbrev = re.findall(r'/if/html/(.*?).html',
+                                        url)[0].lower()
             out_json = '{}/{}.json'.format(out_dir, journal_abbrev)
             args_list.append([url, out_json, tsleep])
 
-    tmp = utils.parallel_call(get_journal_data, args_list, nproc, nretry)
+    args_list = [x for x in sorted(args_list, key=lambda x: x[0])]
+        #     if len(args_list) > 10:
+        #         break
+        # if len(args_list) > 10:
+        #     break
+
+    done_set = set()
+    cycle = 0
+    while True:
+        tmp = utils.parallel_call(get_journal_data, args_list, nproc, nretry)
+        cycle += 1
+
+        todo_set = set()
+        for x in tmp:
+            for url in x['done']:
+                done_set.add(url)
+            for url in x['todo']:
+                todo_set.add(url)
+
+        todo_set = todo_set - done_set
+        if len(todo_set) == 0:
+            break
+
+        args_list = []
+        for url in sorted(todo_set):
+            journal_abbrev = re.findall(r'/if/html/(.*?).html',
+                                        url)[0].lower()
+            out_json = '{}/{}.json'.format(out_dir, journal_abbrev)
+            args_list.append([url, out_json, tsleep])
+
+        utils.qprint('after cycle {}, get {} new journals'
+                     .format(cycle, len(args_list)))
+
+# def catch_missing(timestamp, tsleep=1):
+
+#     subject_json = '../data/{}.subject.json'.format(timestamp)
+#     data = utils.load_json(subject_json)
+#     url_set = set()
+#     for subj in data:
+#         for url in data[subj]:
+#             url_set.add(url)
+
+#     min_url = sorted(url_set)[0]
+#     utils.qprint('starting from ' + min_url)
+
+#     novel_url_set = set()
+#     url = min_url
+#     count = 0
+#     count_max = 100000
+#     while True:
+#         journal_abbrev = re.findall(r'/if/html/(.*?).html', url)[0].lower()
+#         out_json = '../data/{}/'
+#         get_journal_data(next_url, out_json, tsleep)
+
+#         r = requests.get(url)
+#         time.sleep(tsleep)
+#         if not r.ok:
+#             raise '*ERROR*: can not retrieve ' + url
+#         root = etree.HTML(r.text)
+#         a = root.xpath('//div[@class="col-md-6 col-sm-12 text-right"]/a')
+#         next_url = 'http://www.bioxbio.com/if/html/' + a[0].get('href')
+#         if next_url not in url_set:
+#             novel_url_set.add(next_url)
+
+
+
+#         url = next_url
+#         if url == min_url:
+#             break
+#         count += 1
+#         if count == count_max:
+#             break
+
+
+# def get_all_journal_data(all_journal_dict, out_dir,
+#                          nproc=3, nretry=10, tsleep=1):
+#     '''Get data of all journals'''
+
+#     os.makedirs(out_dir, exist_ok=True)
+
+#     args_list = []
+#     for subject in sorted(all_journal_dict):
+#         for url in sorted(all_journal_dict[subject]):
+#             journal_abbrev = re.findall(r'/if/html/(.*?).html', url)[0].lower()
+#             out_json = '{}/{}.json'.format(out_dir, journal_abbrev)
+#             args_list.append([url, out_json, tsleep])
+
+#     tmp = utils.parallel_call(get_journal_data, args_list, nproc, nretry)
+
+def test():
+
+    data, error = get_journal_data(
+        'http://www.bioxbio.com/if/html/Z-PADAGOGIK.html',
+        'foo.json', 0)
+    print(data)
+    print(error)
+
+
 
 if __name__ == '__main__':
 
-    # # in_url = 'http://www.bioxbio.com/if/html/NAT-COMMUN.html'
-    # in_url = sys.argv[1]
-    # out_json = 'foo.json'
-    # tsleep = 0
-    # data, error = get_journal_data(in_url, out_json, tsleep)
-    # print(error)
+    # timestamp = '201804140057'
+    # catch_missing(timestamp)
+    # test()
+
 
     if len(sys.argv) != 2:
         timestamp = utils.get_timestamp()
